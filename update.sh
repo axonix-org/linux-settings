6314c6064bd57d0ef95a8a55c064be181ddc86bb
Andrey <andrey2455@gmail.com>
2024-06-09 12:54:58 +0700

    initial

--- update.sh
+++ update.sh
@@ -28,11 +28,3 @@ wget -P ~/ 'https://github.com/axonix-org/linux-settings/raw/main/.bash_aliases'
 
 # Установка git
 sudo apt install -y git
-# Добавление в .bashrc отображения ветки git в терминале
-echo '' >> ~/.bashrc
-echo 'parse_git_branch() {
-     git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"
-}
-export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bashrc
-# Перезапуск .bashrc
-. ~/.bashrc
